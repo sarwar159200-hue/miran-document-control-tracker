a42e65bd0e8aae59307c5c338affe025b798c07f
sarwar159200-hue <sarwar.159200@gmail.com>
2026-09-02 21:11:21 +0300

    Delete MDCT-V3.1-PST-IMPORT-FIX directory

--- MDCT-V3.1-PST-IMPORT-FIX/lib/supabase-browser.js
+++
@@ -1,2 +0,0 @@
-import {createClient} from "@supabase/supabase-js";
-let c; export function sb(){if(!c)c=createClient(process.env.NEXT_PUBLIC_SUPABASE_URL||"",process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY||"");return c;}
