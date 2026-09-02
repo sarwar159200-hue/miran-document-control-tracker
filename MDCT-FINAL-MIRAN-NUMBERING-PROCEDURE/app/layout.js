59dfa96a8e2b6d3dfff72647908d7d1527de5910
sarwar159200-hue <sarwar.159200@gmail.com>
2026-09-02 13:19:01 +0300

    Delete MDCT-FINAL-MIRAN-NUMBERING-PROCEDURE directory

--- MDCT-FINAL-MIRAN-NUMBERING-PROCEDURE/app/layout.js
+++
@@ -1,3 +0,0 @@
-import "./globals.css";
-export const metadata={title:"Miran Document Control Tracker"};
-export default function RootLayout({children}){return <html><body>{children}</body></html>}
--- MDCT-FINAL-MIRAN-NUMBERING-PROCEDURE/lib/exportExcel.js
+++
@@ -1,2 +0,0 @@
-import * as XLSX from "xlsx";
-export function exportRows(rows){const wb=XLSX.utils.book_new();const ws=XLSX.utils.json_to_sheet(rows);XLSX.utils.book_append_sheet(wb,ws,"Document Register");XLSX.writeFile(wb,"MDCT-Document-Register.xlsx");}
